5de0744cad34dc81e074bbaffea938f379e500e9
寺田恭 <kyo.terada@sellca-sellcar.com>
2025-05-21 10:53:18 +0900

    VO 修正

--- src/domains/value-objects/UserId/index.ts
+++ src/domains/value-objects/UserId/index.ts
@@ -1,21 +1,33 @@
+/**
+ * ユーザーIDを表す値オブジェクト
+ *
+ * @class UserId
+ * @description ユーザーを一意に識別するためのIDを表現する値オブジェクト
+ */
 export class UserId {
-	private _userId: string;
+  private readonly _userId: string;
 
-	constructor(userId: string) {
-		if (!userId) {
-			throw new Error("userId の値が不正です");
-		}
-		this._userId = userId;
-	}
+  constructor(userId: string) {
+    if (!userId) {
+      throw new Error("userId の値が不正です");
+    }
+    this._userId = userId;
+  }
 
-	get value(): string {
-		return this._userId;
-	}
+  /**
+   * 他のUserIdインスタンスとの等価性を比較します
+   *
+   * @param {UserId} other - 比較対象のUserIdインスタンス
+   * @returns {boolean} 等価な場合はtrue、そうでない場合はfalse
+   */
+  public equals(other: UserId): boolean {
+    if (!other) {
+      return false;
+    }
+    return this._userId === other.value;
+  }
 
-	public equals(other: UserId): boolean {
-		if (!other) {
-			return false;
-		}
-		return this._userId === other._userId;
-	}
+  get value(): string {
+    return this._userId;
+  }
 }
--- src/domains/value-objects/UserName/index.ts
+++ src/domains/value-objects/UserName/index.ts
@@ -1,16 +1,36 @@
+/**
+ * ユーザー名を表す値オブジェクト
+ *
+ * @class UserName
+ * @description ユーザーの名前を表現する値オブジェクト
+ */
 export class UserName {
-	private _userName: string;
-	constructor(userName: string) {
-		if (!userName) {
-			throw new Error("userName の値が不正です");
-		}
-		if (userName.length < 1) {
-			throw new Error("userName は2文字以上である必要があります");
-		}
-		this._userName = userName;
-	}
+  private readonly _userName: string;
 
-	get value(): string {
-		return this._userName;
-	}
+  constructor(userName: string) {
+    if (!userName) {
+      throw new Error("userName の値が不正です");
+    }
+    if (userName.length < 1) {
+      throw new Error("userName は1文字以上である必要があります");
+    }
+    this._userName = userName;
+  }
+
+  /**
+   * 他のUserNameインスタンスとの等価性を比較します
+   *
+   * @param {UserName} other - 比較対象のUserNameインスタンス
+   * @returns {boolean} 等価な場合はtrue、そうでない場合はfalse
+   */
+  public equals(other: UserName): boolean {
+    if (!other) {
+      return false;
+    }
+    return this._userName === other.value;
+  }
+
+  get value(): string {
+    return this._userName;
+  }
 }
